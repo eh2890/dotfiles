b5a3d24856e79e2e162c391fe7ebf9508476d5e8
Eumin Hong <milouhong45@gmail.com>
2021-02-18 02:42:10 -0500

    Updated by Eumins-iMac on Thu Feb 18 02:42:10 EST 2021

--- sync.sh
+++ sync.sh
@@ -8,13 +8,11 @@ currentdate=$(date)
 cd ~/personal_settings/
 git pull --ff-only
 
-
 # copying all files to where they are being used
 cp ~/personal_settings/.bashrc ~/.bashrc
 cp ~/personal_settings/.vimrc ~/.vimrc
 cp ~/personal_settings/.vim/plugged/vimtex/ftplugin/tex.vim ~/.vim/plugged/vimtex/ftplugin/tex.vim
 cp -r ~/personal_settings/Library/texmf/tex/latex/local/ ~/Library/texmf/tex/latex/local/
 cp -r ~/personal_settings/.vim/UltiSnips/ ~/.vim/UltiSnips/
-
 
 printf "\n\nSynced by $user on $currentdate\n\n"
