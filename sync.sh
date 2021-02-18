93fc5d1d8d68e4c0d986b21f8f24afe990dc8e45
Eumin Hong <milouhong45@gmail.com>
2021-02-18 02:38:34 -0500

    Updated by Eumins-iMac on Thu Feb 18 02:38:34 EST 2021

---
+++ sync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# getting commit message data
+user="$(cut -d'.' -f1 <<<"${HOSTNAME}")"
+currentdate=$(date)
+
+# pulling
+cd ~/personal_settings/
+git pull
+
+
+# copying all files to where they are being used
+cp ~/personal_settings/.bashrc ~/.bashrc
+cp ~/personal_settings/.vimrc ~/.vimrc
+cp ~/personal_settings/.vim/plugged/vimtex/ftplugin/tex.vim ~/.vim/plugged/vimtex/ftplugin/tex.vim
+cp -r ~/personal_settings/Library/texmf/tex/latex/local/ ~/Library/texmf/tex/latex/local/
+cp -r ~/personal_settings/.vim/UltiSnips/ ~/.vim/UltiSnips/
+
+
+printf "\n\nSynced by $user on $currentdate\n\n"
