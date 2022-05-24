f6b2e2568190ba715f4d3f0f31663a2705c3a474
Eumin Hong <milouhong45@gmail.com>
2022-05-24 09:55:37 -0700

    Updated by MBP on Tue May 24 09:55:37 PDT 2022

--- sync.sh
+++ sync.sh
@@ -11,6 +11,7 @@ git pull --ff-only
 # copying all files to where they are being used
 cp ~/personal_settings/.bashrc ~/.bashrc
 cp ~/personal_settings/.vimrc ~/.vimrc
+cp ~/personal_settings/.profile ~/.profile
 cp ~/personal_settings/.vim/plugged/vimtex/ftplugin/tex.vim ~/.vim/plugged/vimtex/ftplugin/tex.vim
 cp -r ~/personal_settings/Library/texmf/tex/latex/local/ ~/Library/texmf/tex/latex/local/
 cp -r ~/personal_settings/.vim/UltiSnips/ ~/.vim/UltiSnips/
