6d49f9bc5f9ab5d926c3d2f2fddb388f210d0f9f
Eumin Hong <milouhong45@gmail.com>
2024-11-10 22:26:16 -0500

    saving

--- push.sh
+++ push.sh
@@ -5,10 +5,6 @@ user="$(cut -d'.' -f1 <<<"${HOSTNAME}")"
 currentdate=$(date)
 
 # copying all files to ~/personal settings
-cp ~/.bashrc ~/personal_settings/.bashrc
-cp ~/.vimrc ~/personal_settings/.vimrc
-cp ~/.profile ~/personal_settings/.profile
-cp ~/.bash_profile ~/personal_settings/.bash_profile
 cp ~/.vim/plugged/vimtex/ftplugin/tex.vim ~/personal_settings/.vim/plugged/vimtex/ftplugin/tex.vim
 cp -r ~/Library/texmf/tex/latex/local/ ~/personal_settings/Library/texmf/tex/latex/local/
 cp -r ~/.vim/UltiSnips/ ~/personal_settings/.vim/UltiSnips/
--- sync.sh
+++ sync.sh
@@ -9,10 +9,6 @@ cd ~/personal_settings/
 git pull --ff-only
 
 # copying all files to where they are being used
-cp ~/personal_settings/.bashrc ~/.bashrc
-cp ~/personal_settings/.vimrc ~/.vimrc
-cp ~/personal_settings/.profile ~/.profile
-cp ~/personal_settings/.bash_profile ~/.bash_profile
 cp ~/personal_settings/.vim/plugged/vimtex/ftplugin/tex.vim ~/.vim/plugged/vimtex/ftplugin/tex.vim
 cp -r ~/personal_settings/Library/texmf/tex/latex/local/ ~/Library/texmf/tex/latex/local/
 cp -r ~/personal_settings/.vim/UltiSnips/ ~/.vim/UltiSnips/
